26a3b35a76cc84ed9f2b9fb93c6785dd7f0dfbe4
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-08 18:09:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 # ******************************
 # Make your Code
 # ******************************
+import random 
+
+numbers = []
+
+for i in range(10):
+    num = random.randint(0,100)
+    numbers.append(num)
+
+minNum = min(numbers)
+minNumIndex = numbers.index(minNum)
+
+print(minNum)
+print(minNumIndex)
+
